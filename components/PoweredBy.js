16004baf682a9a14fb43d877e040ae32a40ffa9f
chnbsdan <chnbsdan@gmail.com>
2025-11-25 19:49:18 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -1,19 +1,12 @@
-import { siteConfig } from '@/lib/config'
-
-/**
- * 驱动版权
- * @returns
- */
 export default function PoweredBy(props) {
   return (
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
       <a
-        href='https://github.com/chnbsdan/NotionNext'
+        href='https://github.com/chnbsdan'  // 替换为你的官网链接
         className='underline justify-start'>
-        NotionNext {siteConfig('VERSION')}
+        BSDAN智能家居制造商
       </a>
-      .
     </div>
   )
 }
